78cb66497926ce8348e4db10abb8c5bf3e387e9c
erik <erik.terwiel@gmail.com>
2017-08-01 13:14:53 -0700

    Added onClick listeners for save and emojify buttons

--- app/src/main/java/erikterwiel/emojify/MainActivity.java
+++ app/src/main/java/erikterwiel/emojify/MainActivity.java
@@ -2,12 +2,55 @@ package erikterwiel.emojify;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> mSavedTexts;
+
+    private EditText mTextInput;
+    private Button mSaveText;
+    private Button mCopyText;
+    private Button mEmojifyText;
+    private TextView mTextOutput;
+    private RecyclerView mSavedTextList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextInput.findViewById(R.id.main_text_input);
+        mSaveText.findViewById(R.id.main_save_text);
+        mCopyText.findViewById(R.id.main_copy_text);
+        mEmojifyText.findViewById(R.id.main_emojify_text);
+        mTextOutput.findViewById(R.id.main_text_output);
+        mSavedTextList.findViewById(R.id.main_saved_text_list);
+
+        mSaveText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mSavedTexts.add(mTextOutput.getText().toString());
+            }
+        });
+
+        mEmojifyText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                emojifyString(mTextInput.getText().toString());
+            }
+        });
+
+
+    }
+
+    private String emojifyString(String input) {
+        return input;
     }
 }
