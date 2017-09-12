82ee15f243f62ef744e2419c71bc0c1bd83ebbeb
erikterwiel <erik.terwiel@gmail.com>
2017-09-12 17:23:09 -0400

    Added new layouts

--- app/src/main/java/erikterwiel/emojify/MainActivity.java
+++ app/src/main/java/erikterwiel/emojify/MainActivity.java
@@ -6,7 +6,9 @@ import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -19,14 +21,15 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private EmojiHashMap mDatabase;
-    private ArrayList<String> mSavedTexts;
+    private ArrayList<String> mRecentTexts;
+    private RecentAdapter mRecentAdapter;
 
     private EditText mTextInput;
     private Button mSaveText;
     private Button mCopyText;
     private Button mEmojifyText;
     private TextView mTextOutput;
-    private RecyclerView mSavedTextList;
+    private RecyclerView mRecentTextList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
         mCopyText = (Button) findViewById(R.id.main_copy_text);
         mEmojifyText = (Button) findViewById(R.id.main_emojify_text);
         mTextOutput = (TextView) findViewById(R.id.main_text_output);
-        mSavedTextList = (RecyclerView) findViewById(R.id.main_saved_text_list);
+        mRecentTextList = (RecyclerView) findViewById(R.id.main_recent_text_list);
 
         // Saves texts to saved texts list on click
         mSaveText.setOnClickListener(new View.OnClickListener() {
@@ -76,8 +79,11 @@ public class MainActivity extends AppCompatActivity {
                 mTextOutput.setText(output);
             }
         });
+    }
 
-
+    private void updateUI() {
+        mRecentAdapter = new RecentAdapter(mRecentTexts);
+        mRecentTextList.setAdapter(mRecentAdapter);
     }
 
     private String emojifyString(String input) {
@@ -103,5 +109,23 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         return output;
+    }
+
+    private class RecentAdapter extends RecyclerView.Adapter<RecentHolder> {
+        private ArrayList<String> recentTexts;
+
+        private RecentAdapter(ArrayList<String> texts) {
+            recentTexts = texts;
+        }
+
+        @Override
+        public RecentHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(MainActivity.this);
+            View view = layoutInflater.inflate(R.layout)
+        }
+    }
+
+    private class RecentHolder extends RecyclerView.ViewHolder {
+
     }
 }
