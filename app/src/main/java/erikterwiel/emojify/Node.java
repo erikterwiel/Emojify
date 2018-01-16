ed8f43af835b16268108effbcc05dbe04b1c7e4c
ehterwie <ehterwie@uwaterloo.ca>
2018-01-16 03:20:13 -0500

    Correctly implemented Node class

--- app/src/main/java/erikterwiel/emojify/Node.java
+++ app/src/main/java/erikterwiel/emojify/Node.java
@@ -1,6 +1,22 @@
 package erikterwiel.emojify;
 
 public class Node {
-    String item;
-    Node next;
+    private String mItem;
+    private Node mNext;
+
+    public String getItem() {
+        return mItem;
+    }
+
+    public void setItem(String item) {
+        mItem = item;
+    }
+
+    public Node getNext() {
+        return mNext;
+    }
+
+    public void setNext(Node next) {
+        mNext = next;
+    }
 }
