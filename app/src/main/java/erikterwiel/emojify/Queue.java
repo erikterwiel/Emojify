af53f03da4c539924f1f3e3d78b1581696c2a29f
ehterwie <ehterwie@uwaterloo.ca>
2018-01-16 03:25:59 -0500

    Implemented Queue class

--- app/src/main/java/erikterwiel/emojify/Queue.java
+++ app/src/main/java/erikterwiel/emojify/Queue.java
@@ -1,4 +1,43 @@
 package erikterwiel.emojify;
 
 public class Queue {
+
+    public Queue(int capacity) {
+        this.capacity = capacity;
+    }
+
+    private Node first;
+    private Node last;
+    private int n;
+    private int capacity;
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public int size() {
+        return n;
+    }
+
+    public void enqueue(String item) {
+        if (n == capacity) {
+            first = first.getNext();
+            Node oldLast = last;
+            last = new Node();
+            last.setItem(item);
+            last.setNext(null);
+            oldLast.setNext(last);
+        } else {
+            Node oldLast = last;
+            last = new Node();
+            last.setItem(item);
+            last.setNext(null);
+            if (isEmpty()) {
+                first = last;
+            } else {
+                oldLast.setNext(last);
+            }
+            n += 1;
+        }
+    }
 }
